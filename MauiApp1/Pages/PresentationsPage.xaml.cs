4d390b48910e8ea23f582b979dea5ab0ae4eb27b
PedroOsorioo <osorioemiliano66@gmail.com>
2026-02-15 12:34:21 -0600

    Experimentos
    Se retiraron etiquetas sobre los labels con el fin de mantener limpio el espacio de trabajo. Se hicieron diversas pruebas para innovar el diseño de las presentaciones, esto con el uso de grid, columnas y filas. No se concretaron las pruebas con el grid por diversos errores que acontecieron, se usará una versión más elaborada de la usada en la pantalla de discografías.

--- MauiApp1/Pages/PresentationsPage.xaml.cs
+++ MauiApp1/Pages/PresentationsPage.xaml.cs
@@ -1,9 +1,28 @@
+using MauiApp1.Models;
+
 namespace MauiApp1.Pages;
 
 public partial class PresentationsPage : ContentPage
 {
-	public PresentationsPage()
+
+    private List<PresentationsModel> _presentations = [
+
+        new() { Url = "https://i.scdn.co/image/ab67616d0000b2731624590458126fc8b8c64c2f", Name = "Cruel World",Description = "Álbum Ultraviolence en su versión deluxe de la cantante Lana Del Rey"  }
+        
+        
+        
+    ];
+
+    private List<PresentationsModel> _filteredPresentations;
+    public PresentationsPage()
 	{
 		InitializeComponent();
-	}
+        _filteredPresentations = [.. _presentations];
+        presentationsList.ItemsSource = _filteredPresentations;
+    }
+
+    private void OnContactSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+
+    }
 }
