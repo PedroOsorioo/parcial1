f1446e6fc4a10767659a1f05f737a5d4b74786a9
SparkleJumpRopeQueen85 <sch.samantha0504@gmail.com>
2026-02-17 00:52:07 -0600

    Add base files MAUI program
    Archivos de la rama de Samantha

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,15 +1,13 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Pages;
+namespace MauiApp1
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-        }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new MainPage();
         }
     }
 }
---
+++ MauiApp1/Pages/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Pages;
+
+public partial class MainPage : TabbedPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
